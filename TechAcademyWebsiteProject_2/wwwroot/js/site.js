8345ae1c41e48f4190eacce03400780f04ab08fb
Matt <87787075+maty714@users.noreply.github.com>
2021-12-24 17:54:45 -0500

    Added CSS, JS, HTML, and images

--- TechAcademyWebsiteProject_2/wwwroot/js/site.js
+++ TechAcademyWebsiteProject_2/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = x.length };
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+}
